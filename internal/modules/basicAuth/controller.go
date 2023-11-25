84bf717d7450f16b2e8d0032aa5363779e56b41a
Danis Abadia <deanis89@hotmail.com>
2023-11-25 11:37:41 -0500

    FIX: Bug in controller

--- internal/modules/basicAuth/controller.go
+++ internal/modules/basicAuth/controller.go
@@ -2,6 +2,7 @@ package basicAuth
 
 import (
 	"com.github/mrthoabby/m-authentication/helpers"
+	"com.github/mrthoabby/m-authentication/util"
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,8 +14,9 @@ func AuthController(group *gin.RouterGroup) {
 
 func login(context *gin.Context) {
 	var crendentials Credentials
-	contentType := context.ContentType()
+	contentType := context.Request.Header.Get("Content-Type")
 	if errorGettingData := helpers.Binder[Credentials](contentType, context, &crendentials); errorGettingData != nil {
+		util.LoggerHandler().Error("Error getting data", "error", errorGettingData.Error())
 		context.JSON(400, gin.H{"message": errorGettingData.Error()})
 		return
 	}
